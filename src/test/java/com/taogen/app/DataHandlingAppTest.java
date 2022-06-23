c07ba0268453c840aba84265dd91a4de93c76ae9
taogen <taogenjia@gmail.com>
2022-06-23 16:31:31 +0800

    [chore] Build maven project

---
+++ src/test/java/com/taogen/app/DataHandlingAppTest.java
@@ -0,0 +1,19 @@
+package com.taogen.app;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class DataHandlingAppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
