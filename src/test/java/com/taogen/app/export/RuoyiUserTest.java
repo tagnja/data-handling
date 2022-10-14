cd6f3b7129943125e8fbd5ff7e3805f869a014e9
taogen <taogenjia@gmail.com>
2022-10-14 16:08:58 +0800

    [test] Optimize code

--- src/test/java/com/taogen/app/export/RuoyiUserTest.java
+++ src/test/java/com/taogen/app/export/RuoyiUserTest.java
@@ -11,12 +11,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Taogen
@@ -41,46 +45,69 @@ public class RuoyiUserTest extends ExportBaseTest {
             Iterator<Row> iterator = sheet.iterator();
             iterator.next();
             Row row;
+            List<String> sqlList = new ArrayList<>();
             while (iterator.hasNext()) {
                 row = iterator.next();
                 DataFormatter formatter = new DataFormatter();
-                String name = formatter.formatCellValue(row.getCell(5));
-                String passwd = formatter.formatCellValue(row.getCell(6));
-                String leader = formatter.formatCellValue(row.getCell(3));
-                String phone = formatter.formatCellValue(row.getCell(4));
+                String name = formatter.formatCellValue(row.getCell(5)).trim();
+                String passwd = formatter.formatCellValue(row.getCell(6)).trim();
+                String leader = formatter.formatCellValue(row.getCell(3)).trim();
+                String phone = formatter.formatCellValue(row.getCell(4)).trim();
                 if (StringUtils.isNotEmpty(phone) && phone.indexOf("、") > 0) {
                     phone = phone.substring(0, phone.indexOf("、"));
                 }
+                log.debug("name is {}", name);
                 if (doesUserExist(name)) {
-                    System.out.println(name + "已存在！！");
+                    log.info("{} already exists!", name);
                     continue;
                 }
-                // log.debug(name);
-                // log.debug(passwd);
-                String insertDeptSql = "insert into sys_dept " +
-                        " (parent_id, ancestors, dept_name, order_num, leader, phone, email, create_by) " +
-                        " value (100, '0,100', '${name}', 1, '${leader}', '${phone}', '', 'admin'); ";
-                System.out.println(insertDeptSql
-                        .replace("${name}", name)
-                        .replace("${leader}", leader)
-                        .replace("${phone}", phone));
-                System.out.println("SELECT @deptId := LAST_INSERT_ID();");
-                String insertUserSql = "insert into sys_user " +
-                        " (dept_id, user_name, nick_name, phonenumber, password, create_by, words_left, yuqing_group_id) " +
-                        " value (@deptId, '${name}', '${name}', '${phone}', '${passwd}', 'admin', '30000', '214');";
-                System.out.println(insertUserSql
-                        .replace("${name}", name)
-                        .replace("${passwd}", encryptPassword(passwd))
-                        .replace("${leader}", leader)
-                        .replace("${phone}", phone));
-                System.out.println("SELECT @userId := LAST_INSERT_ID();");
-                String insertUserRoleSql = "insert into sys_user_role " +
-                        " (user_id, role_id) " +
-                        " value (@userId, 2);";
-                System.out.println(insertUserRoleSql);
-                System.out.println();
+                sqlList.addAll(getExecSqlList(name, passwd, phone, leader));
+            }
+            log.info("sql list size is {}", sqlList.size());
+            log.info("sql list is \n{}", sqlList.stream().collect(Collectors.joining("\r\n")));
+            boolean execute = false;
+            if (execute) {
+                execSqlList(sqlList);
             }
         }
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    void execSqlList(List<String> sqlList) {
+        for (String sql : sqlList) {
+            log.debug("sql is {}", sql);
+            jdbcTemplate.execute(sql);
+        }
+    }
+
+    private List<String> getExecSqlList(String name, String passwd, String phone, String leader) {
+        List<String> sqlList = new ArrayList<>();
+        String insertDeptSql = "insert into sys_dept " +
+                " (parent_id, ancestors, dept_name, order_num, leader, phone, email, create_by) " +
+                " value (100, '0,100', '${name}', 1, '${leader}', '${phone}', '', 'admin'); ";
+        insertDeptSql = insertDeptSql
+                .replace("${name}", name)
+                .replace("${leader}", leader)
+                .replace("${phone}", phone);
+        sqlList.add(insertDeptSql);
+        String deptIdSql = "SELECT @deptId := LAST_INSERT_ID();";
+        sqlList.add(deptIdSql);
+        String insertUserSql = "insert into sys_user " +
+                " (dept_id, user_name, nick_name, phonenumber, password, create_by, words_left, yuqing_group_id) " +
+                " value (@deptId, '${name}', '${name}', '${phone}', '${passwd}', 'admin', '30000', '214');";
+        insertUserSql = insertUserSql
+                .replace("${name}", name)
+                .replace("${passwd}", encryptPassword(passwd))
+                .replace("${leader}", leader)
+                .replace("${phone}", phone);
+        sqlList.add(insertUserSql);
+        String userIdSql = "SELECT @userId := LAST_INSERT_ID();";
+        sqlList.add(userIdSql);
+        String insertUserRoleSql = "insert into sys_user_role " +
+                " (user_id, role_id) " +
+                " value (@userId, 2);";
+        sqlList.add(insertUserRoleSql);
+        return sqlList;
     }
 
     private void updateUserPassword(String username, String passwd) {
