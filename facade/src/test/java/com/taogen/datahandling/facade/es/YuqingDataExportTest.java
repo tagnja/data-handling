80f13a04ed13445c631951c7e110e283e17c01a5
taogen <taogenjia@gmail.com>
2023-09-22 18:51:05 +0800

    [test] Optimize code

--- facade/src/test/java/com/taogen/datahandling/facade/es/YuqingDataExportTest.java
+++ facade/src/test/java/com/taogen/datahandling/facade/es/YuqingDataExportTest.java
@@ -95,12 +95,20 @@ class YuqingDataExportTest {
     private String buildDslStr(List<EsFieldInfo> queryFields,
                                Map<EsFieldInfo, Object[]> queryConditions,
                                String keywordExpression,
-                               int size) {
+                               Integer size,
+                               String orderField,
+                               SortOrder sortOrder) {
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.size(size);
-        searchSourceBuilder.sort("pub_time", SortOrder.DESC);
-        List<String> fetchFields = queryFields.stream().map(EsFieldInfo::getQueryField).distinct().collect(Collectors.toList());
-        searchSourceBuilder.fetchSource(fetchFields.toArray(new String[0]), null);
+        if (queryFields != null) {
+            List<String> fetchFields = queryFields.stream().map(EsFieldInfo::getQueryField).distinct().collect(Collectors.toList());
+            searchSourceBuilder.fetchSource(fetchFields.toArray(new String[0]), null);
+        }
+        if (size != null) {
+            searchSourceBuilder.size(size);
+        }
+        if (orderField != null && sortOrder != null) {
+            searchSourceBuilder.sort(orderField, sortOrder);
+        }
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
         // 非垃圾数据（默认条件，和舆情搜索保持一致）
         boolQueryBuilder.must(QueryBuilders.termQuery(EsField.STATUS, "0"));
@@ -108,10 +116,12 @@ class YuqingDataExportTest {
         if (keywordBoolQuery != null) {
             boolQueryBuilder.must(keywordBoolQuery);
         }
-        for (Map.Entry<EsFieldInfo, Object[]> entry : queryConditions.entrySet()) {
-            EsFieldInfo esFieldInfo = entry.getKey();
-            Object[] values = entry.getValue();
-            boolQueryBuilder.must(esFieldInfo.getQueryFunction().apply(values));
+        if (queryConditions != null) {
+            for (Map.Entry<EsFieldInfo, Object[]> entry : queryConditions.entrySet()) {
+                EsFieldInfo esFieldInfo = entry.getKey();
+                Object[] values = entry.getValue();
+                boolQueryBuilder.must(esFieldInfo.getQueryFunction().apply(values));
+            }
         }
         searchSourceBuilder.query(boolQueryBuilder);
         log.debug("searchSourceBuilder: {}", searchSourceBuilder);
