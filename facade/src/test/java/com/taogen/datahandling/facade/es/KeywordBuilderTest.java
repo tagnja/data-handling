a0b6350378a2ea7280f0b5d4d2ec01c866515c1b
taogen <taogenjia@gmail.com>
2023-09-23 12:58:58 +0800

    [test] Add tests of KeywordBuilder

--- facade/src/test/java/com/taogen/datahandling/facade/es/KeywordBuilderTest.java
+++ facade/src/test/java/com/taogen/datahandling/facade/es/KeywordBuilderTest.java
@@ -525,4 +525,225 @@ class KeywordBuilderTest {
         assertEquals(expect, boolQueryBuilderByExpression.toString());
     }
 
+    @Test
+    void getBoolQueryBuilderByExpression_mix_2() {
+        String keywordExpression = "(a+b-c)+(d|e)-(f-g)"; // equals (a+b+(d|e))-c-(f-g)
+        BoolQueryBuilder boolQueryBuilderByExpression = KeywordBuilder.getBoolQueryBuilderByExpression(keywordExpression);
+        System.out.println(boolQueryBuilderByExpression);
+        String expect = "{\n" +
+                "  \"bool\" : {\n" +
+                "    \"must\" : [\n" +
+                "      {\n" +
+                "        \"bool\" : {\n" +
+                "          \"should\" : [\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"title\" : {\n" +
+                "                  \"query\" : \"a\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"content\" : {\n" +
+                "                  \"query\" : \"a\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"adjust_pure_negative\" : true,\n" +
+                "          \"minimum_should_match\" : \"1\",\n" +
+                "          \"boost\" : 1.0\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"bool\" : {\n" +
+                "          \"should\" : [\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"title\" : {\n" +
+                "                  \"query\" : \"b\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"content\" : {\n" +
+                "                  \"query\" : \"b\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"adjust_pure_negative\" : true,\n" +
+                "          \"minimum_should_match\" : \"1\",\n" +
+                "          \"boost\" : 1.0\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"bool\" : {\n" +
+                "          \"should\" : [\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"title\" : {\n" +
+                "                  \"query\" : \"d\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"content\" : {\n" +
+                "                  \"query\" : \"d\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"title\" : {\n" +
+                "                  \"query\" : \"e\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"content\" : {\n" +
+                "                  \"query\" : \"e\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"adjust_pure_negative\" : true,\n" +
+                "          \"minimum_should_match\" : \"1\",\n" +
+                "          \"boost\" : 1.0\n" +
+                "        }\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"must_not\" : [\n" +
+                "      {\n" +
+                "        \"bool\" : {\n" +
+                "          \"should\" : [\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"title\" : {\n" +
+                "                  \"query\" : \"c\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"match_phrase\" : {\n" +
+                "                \"content\" : {\n" +
+                "                  \"query\" : \"c\",\n" +
+                "                  \"slop\" : 0,\n" +
+                "                  \"zero_terms_query\" : \"NONE\",\n" +
+                "                  \"boost\" : 1.0\n" +
+                "                }\n" +
+                "              }\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"adjust_pure_negative\" : true,\n" +
+                "          \"minimum_should_match\" : \"1\",\n" +
+                "          \"boost\" : 1.0\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"bool\" : {\n" +
+                "          \"must\" : [\n" +
+                "            {\n" +
+                "              \"bool\" : {\n" +
+                "                \"should\" : [\n" +
+                "                  {\n" +
+                "                    \"match_phrase\" : {\n" +
+                "                      \"title\" : {\n" +
+                "                        \"query\" : \"f\",\n" +
+                "                        \"slop\" : 0,\n" +
+                "                        \"zero_terms_query\" : \"NONE\",\n" +
+                "                        \"boost\" : 1.0\n" +
+                "                      }\n" +
+                "                    }\n" +
+                "                  },\n" +
+                "                  {\n" +
+                "                    \"match_phrase\" : {\n" +
+                "                      \"content\" : {\n" +
+                "                        \"query\" : \"f\",\n" +
+                "                        \"slop\" : 0,\n" +
+                "                        \"zero_terms_query\" : \"NONE\",\n" +
+                "                        \"boost\" : 1.0\n" +
+                "                      }\n" +
+                "                    }\n" +
+                "                  }\n" +
+                "                ],\n" +
+                "                \"adjust_pure_negative\" : true,\n" +
+                "                \"minimum_should_match\" : \"1\",\n" +
+                "                \"boost\" : 1.0\n" +
+                "              }\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"must_not\" : [\n" +
+                "            {\n" +
+                "              \"bool\" : {\n" +
+                "                \"should\" : [\n" +
+                "                  {\n" +
+                "                    \"match_phrase\" : {\n" +
+                "                      \"title\" : {\n" +
+                "                        \"query\" : \"g\",\n" +
+                "                        \"slop\" : 0,\n" +
+                "                        \"zero_terms_query\" : \"NONE\",\n" +
+                "                        \"boost\" : 1.0\n" +
+                "                      }\n" +
+                "                    }\n" +
+                "                  },\n" +
+                "                  {\n" +
+                "                    \"match_phrase\" : {\n" +
+                "                      \"content\" : {\n" +
+                "                        \"query\" : \"g\",\n" +
+                "                        \"slop\" : 0,\n" +
+                "                        \"zero_terms_query\" : \"NONE\",\n" +
+                "                        \"boost\" : 1.0\n" +
+                "                      }\n" +
+                "                    }\n" +
+                "                  }\n" +
+                "                ],\n" +
+                "                \"adjust_pure_negative\" : true,\n" +
+                "                \"minimum_should_match\" : \"1\",\n" +
+                "                \"boost\" : 1.0\n" +
+                "              }\n" +
+                "            }\n" +
+                "          ],\n" +
+                "          \"adjust_pure_negative\" : true,\n" +
+                "          \"boost\" : 1.0\n" +
+                "        }\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"adjust_pure_negative\" : true,\n" +
+                "    \"boost\" : 1.0\n" +
+                "  }\n" +
+                "}";
+        assertEquals(expect, boolQueryBuilderByExpression.toString());
+    }
 }
