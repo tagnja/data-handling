d08ce44db3f570c41e55550033c86a484d0f6950
taogen <taogenjia@gmail.com>
2023-10-07 22:56:14 +0800

    [test] Optimize code

--- facade/src/test/java/com/taogen/datahandling/facade/ExamineDepositRecordExportTest.java
+++ facade/src/test/java/com/taogen/datahandling/facade/ExamineDepositRecordExportTest.java
@@ -7,12 +7,11 @@ import com.taogen.datahandling.mysql.vo.SqlQueryParam;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,12 +26,13 @@ class ExamineDepositRecordExportTest extends ExportBaseTest {
         showConfig();
     }
 
-    @Test
-    void exportExamineDeposit() throws IOException {
-        List<Integer> deptIds = Arrays.asList(214, 267, 485);
-        String beginTime = "2023-09-01 00:00:00";
-        String endTime = "2023-09-31 23:59:59";
-
+    @ParameterizedTest
+    @CsvSource({
+            "214, 2023-09-01 00:00:00, 2023-09-31 23:59:59",
+            "267, 2023-09-01 00:00:00, 2023-09-31 23:59:59",
+            "485, 2023-09-01 00:00:00, 2023-09-31 23:59:59"
+    })
+    void exportExamineDeposit(int deptId, String beginTime, String endTime) throws IOException {
         String sql = "select dept.dept_name as \"客户名称\", user.user_name \"账号名称\", count(*) \"消费次数\", SUM(LENGTH) as \"消费字数\"\n" +
                 "from examine.examine_deposit_record as deposit \n" +
                 "left join examine.sys_dept as dept on deposit.dept_id = dept.dept_id\n" +
@@ -43,18 +43,16 @@ class ExamineDepositRecordExportTest extends ExportBaseTest {
                 "deposit.`length` < 0 and \n" +
                 "deposit.create_time between ? and ?\n" +
                 "group by deposit.create_by";
-        for (Integer deptId : deptIds) {
-            String selectDeptNameSql = "select dept_name from examine.sys_dept where dept_id=?";
-            Map<String, Object> deptNameMap = jdbcTemplate.queryForMap(selectDeptNameSql, deptId);
-            String deptName = deptNameMap.get("dept_name").toString();
-            SqlQueryParam sqlQueryParam = new SqlQueryParam();
-            sqlQueryParam.setSql(sql);
-            sqlQueryParam.setArgs(new Object[]{deptId, beginTime, endTime});
-            LabelAndData labelAndData = mysqlReader.read(jdbcTemplate, sqlQueryParam);
-            String outputPath = getExportDirPath() + File.separator + deptName + ".xlsx";
-            outputPath = FileUtils.appendDateTimeToFileName(outputPath);
-            log.debug("outputPath is: {}", outputPath);
-            excelWriter.writeLabelAndDataToExcel(labelAndData, outputPath);
-        }
+        String selectDeptNameSql = "select dept_name from examine.sys_dept where dept_id=?";
+        Map<String, Object> deptNameMap = jdbcTemplate.queryForMap(selectDeptNameSql, deptId);
+        String deptName = deptNameMap.get("dept_name").toString();
+        SqlQueryParam sqlQueryParam = new SqlQueryParam();
+        sqlQueryParam.setSql(sql);
+        sqlQueryParam.setArgs(new Object[]{deptId, beginTime, endTime});
+        LabelAndData labelAndData = mysqlReader.read(jdbcTemplate, sqlQueryParam);
+        String outputPath = getExportDirPath() + File.separator + deptName + ".xlsx";
+        outputPath = FileUtils.appendDateTimeToFileName(outputPath);
+        log.debug("outputPath is: {}", outputPath);
+        excelWriter.writeLabelAndDataToExcel(labelAndData, outputPath);
     }
 }
