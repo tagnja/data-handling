cf84d2a4007e34aa1d9e3e2b26ababc9ddfbf130
taogen <taogenjia@gmail.com>
2023-10-07 22:44:54 +0800

    [test] Optimize code

--- facade/src/test/java/com/taogen/datahandling/facade/ExamineDepositRecordExportTest.java
+++ facade/src/test/java/com/taogen/datahandling/facade/ExamineDepositRecordExportTest.java
@@ -32,20 +32,21 @@ class ExamineDepositRecordExportTest extends ExportBaseTest {
         List<Integer> deptIds = Arrays.asList(214, 267, 485);
         String beginTime = "2023-09-01 00:00:00";
         String endTime = "2023-09-31 23:59:59";
+
+        String sql = "select dept.dept_name as \"客户名称\", user.user_name \"账号名称\", count(*) \"消费次数\", SUM(LENGTH) as \"消费字数\"\n" +
+                "from examine.examine_deposit_record as deposit \n" +
+                "left join examine.sys_dept as dept on deposit.dept_id = dept.dept_id\n" +
+                "left join examine.sys_user as user on deposit.create_by = user.user_id\n" +
+                "WHERE\n" +
+                "deposit.type in (1, 2, 5) and /*1在线审核，2文档审核，5智能改写*/\n" +
+                "deposit.dept_id=? and \n" +
+                "deposit.`length` < 0 and \n" +
+                "deposit.create_time between ? and ?\n" +
+                "group by deposit.create_by";
         for (Integer deptId : deptIds) {
             String selectDeptNameSql = "select dept_name from examine.sys_dept where dept_id=?";
             Map<String, Object> deptNameMap = jdbcTemplate.queryForMap(selectDeptNameSql, deptId);
             String deptName = deptNameMap.get("dept_name").toString();
-            String sql = "select dept.dept_name as \"客户名称\", user.user_name \"账号名称\", count(*) \"消费次数\", SUM(LENGTH) as \"消费字数\"\n" +
-                    "from examine.examine_deposit_record as deposit \n" +
-                    "left join examine.sys_dept as dept on deposit.dept_id = dept.dept_id\n" +
-                    "left join examine.sys_user as user on deposit.create_by = user.user_id\n" +
-                    "WHERE\n" +
-                    "deposit.type in (1, 2, 5) and /*1在线审核，2文档审核，5智能改写*/\n" +
-                    "deposit.dept_id=? and \n" +
-                    "deposit.`length` < 0 and \n" +
-                    "deposit.create_time between ? and ?\n" +
-                    "group by deposit.create_by";
             SqlQueryParam sqlQueryParam = new SqlQueryParam();
             sqlQueryParam.setSql(sql);
             sqlQueryParam.setArgs(new Object[]{deptId, beginTime, endTime});
