a7f8dcba02b6a55c1a371c943bb592c09f2da6cc
taogen <taogenjia@gmail.com>
2024-01-22 15:56:49 +0800

    ♻️refactor: Add missing commit file

---
+++ data-office/src/test/java/com/taogen/datahandling/office/DataHandlerApplication.java
@@ -0,0 +1,14 @@
+package com.taogen.datahandling.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author taogen
+ */
+@SpringBootApplication
+public class DataHandlerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataHandlerApplication.class, args);
+    }
+}
