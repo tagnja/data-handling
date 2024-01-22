f1b8361ec6c3d98a3185acf27a9e10e837989d8f
taogen <taogenjia@gmail.com>
2024-01-22 16:15:00 +0800

    🚚refactor: Update directory

--- facade/src/test/java/com/taogen/datahandling/facade/mysql/examine/RecoveryDataExportTest.java
+++ facade/src/test/java/com/taogen/datahandling/facade/mysql/examine/RecoveryDataExportTest.java
@@ -1,4 +1,4 @@
-package com.taogen.datahandling.facade;
+package com.taogen.datahandling.facade.mysql.examine;
 
 import com.taogen.commons.datatypes.string.StringUtils;
 import com.taogen.datahandling.common.vo.LabelAndData;
--- facade/src/test/java/com/taogen/datahandling/facade/mysql/examine/RecoveryDataPushWordTest.java
+++ facade/src/test/java/com/taogen/datahandling/facade/mysql/examine/RecoveryDataPushWordTest.java
@@ -1,4 +1,4 @@
-package com.taogen.datahandling.facade;
+package com.taogen.datahandling.facade.mysql.examine;
 
 import com.taogen.commons.datatypes.string.StringUtils;
 import com.taogen.commons.io.FileUtils;
