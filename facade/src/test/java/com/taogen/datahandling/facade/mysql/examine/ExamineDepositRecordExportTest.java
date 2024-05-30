79fd5cd63c8768046d28d2a9a5d864ceb51f9a29
taogen <taogenjia@gmail.com>
2024-05-30 10:13:10 +0800

    ✨refactor: Add exportExamineDepositTongzhou

--- facade/src/test/java/com/taogen/datahandling/facade/mysql/examine/ExamineDepositRecordExportTest.java
+++ facade/src/test/java/com/taogen/datahandling/facade/mysql/examine/ExamineDepositRecordExportTest.java
@@ -5,6 +5,9 @@ import com.taogen.datahandling.common.vo.LabelAndData;
 import com.taogen.datahandling.facade.base.ExportBaseTest;
 import com.taogen.datahandling.mysql.vo.SqlQueryParam;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,6 +15,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * @author Taogen
@@ -53,5 +57,42 @@ class ExamineDepositRecordExportTest extends ExportBaseTest {
         outputPath = FileUtils.appendDateTimeToFileName(outputPath);
         log.debug("outputPath is: {}", outputPath);
         excelWriter.writeLabelAndDataToExcel(labelAndData, outputPath);
+    }
+
+    @ParameterizedTest
+    @Disabled
+    @CsvSource("2024-05-01 00:00:00, 2024-05-31 23:59:59")
+    void exportExamineDepositTongzhou(String startTime, String endTime) throws IOException {
+        String inputFilePath = getExportDirPath() + "南通通州教体局-accounts_2024-03-18_standard.xlsx";
+        Integer userNameColNum = 1;
+        Integer appendUseCountCol = 2;
+        Integer appendUseCharNumCol = 3;
+        DataFormatter formatter = new DataFormatter();
+        Consumer<Row> rowsModifyConsumer = row -> {
+            Cell userNameCell = row.getCell(userNameColNum);
+            String userName = formatter.formatCellValue(userNameCell).trim();
+            String query = "select user.user_name \"账号名称\", count(*) \"消费次数\", SUM(LENGTH) as \"消费字数\"\n" +
+                    "from examine.examine_deposit_record as deposit \n" +
+                    "right join examine.sys_user as user on deposit.create_by = user.user_id\n" +
+                    "WHERE\n" +
+                    "deposit.create_time between '" + startTime + "' and '" + endTime + "' and \n" +
+                    "deposit.type in (1, 2, 5) and\n" +
+                    "deposit.`length` < 0 and \n" +
+                    "user.user_name = \"" + userName + "\"";
+            Map<String, Object> result = jdbcTemplate.queryForMap(query);
+
+            if (result.get("消费次数") != null) {
+                row.createCell(appendUseCountCol).setCellValue(result.get("消费次数").toString());
+            } else {
+                row.createCell(appendUseCountCol).setCellValue(0);
+            }
+            if (result.get("消费字数") != null) {
+                row.createCell(appendUseCharNumCol).setCellValue(result.get("消费字数").toString());
+            } else {
+                row.createCell(appendUseCharNumCol).setCellValue(0);
+            }
+        };
+        String outputFile = excelModifier.modifyRows(inputFilePath, 1, rowsModifyConsumer);
+        log.info("output file: {}", outputFile);
     }
 }
